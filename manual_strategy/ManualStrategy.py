7a9a1cad186dac895ddb990bcb6e06a4ed4bce12
Allan Reyes <reyallan@gatech.edu>
2017-11-03 19:15:39 -0700

    Add ManualStrategy template for manual_strategy

---
+++ manual_strategy/ManualStrategy.py
@@ -0,0 +1,20 @@
+'''
+Implements a ManualStrategy object
+'''
+
+class ManualStrategy(object):
+    '''
+    Implements your manual strategy
+    '''
+
+    def testPolicy(self):
+        '''
+        Returns a trades dataframe
+        '''
+        raise NotImplementedError('Method must be implemented!')
+
+if __name__ == '__main__':
+    # Call `marketsimcode` as necessary
+    # to generate the plots used in the
+    # report
+    pass
